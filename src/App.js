3fed302ffa2ae484e0ab5ee8473bc5c2272031f6
nishant1997 <nishanttelange1997.com>
2021-02-22 08:11:49 +0530

    Bootstrap-Grid

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './Design.css';
+
+function App() {
+  return (
+  <div className="row">
+<div className="fordiv1 col-lg-4 col-sm-12"></div>
+<div className="fordiv2  col-lg-4  col-sm-12"></div>
+<div className="fordiv3  col-lg-4  col-sm-12"></div>
+    </div>
+  );
+}
+
+export default App;
